998fd3fc390526934158d454e25a9dc3b8e1a969
eccedentesiast-kid <kid.nguyenlenganha@gmail.com>
2023-12-30 17:21:58 +0100

    Full search feature

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -8,6 +8,8 @@ $routes = new RouteCollection();
 $routes->add('home', new Route('/'));
 $routes->add('contact', new Route('/contact-to-email'));
 $routes->add('about', new Route('/about'));
+$routes->add('search', new Route('/search'));
+$routes->add('bookSearch', new Route('/bookSearch'));
 $routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
 $routes->add('fibonacci', new Route('/fibonacci/{nb}', ['nb' => 10], ['nb' => '^[1-9][0-9]*$']));
 $routes->add('studentIndex', new Route('/student'));
